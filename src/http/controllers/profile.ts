1943a080b1bc00146da35e8bc1bf53679e4821e8
Rafael Neves <nevesrafael.dev@gmail.com>
2024-05-15 20:56:54 -0300

    :sparkles: Feat authenticate with JWT

---
+++ src/http/controllers/profile.ts
@@ -0,0 +1,7 @@
+import { FastifyRequest, FastifyReply } from "fastify";
+
+export async function profile(request: FastifyRequest, reply: FastifyReply) {
+  await request.jwtVerify();
+
+  return reply.status(200).send(request.user.sub);
+}
