1c826e7afb35c2d30fb0375a817512d5c23c7be2
Rafael Neves <nevesrafael.dev@gmail.com>
2024-05-15 21:43:33 -0300

    :sparkles: Feat profile controller

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,7 +1,17 @@
 import { FastifyRequest, FastifyReply } from "fastify";
+import { makeGetUserProfileService } from "../../services/factories/make-get-user-profile-service";
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
-  await request.jwtVerify();
+  const getUserProfile = makeGetUserProfileService();
 
-  return reply.status(200).send(request.user.sub);
+  const { user } = await getUserProfile.execute({
+    userId: request.user.sub,
+  });
+
+  return reply.status(200).send({
+    user: {
+      ...user,
+      password_hash: undefined,
+    },
+  });
 }
