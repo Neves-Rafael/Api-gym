52632d2563d7c8916ee1d4ef12f7356c2946c4c0
Rafael Neves <nevesrafael.dev@gmail.com>
2024-05-18 22:37:44 -0300

    :sparkles: Feat authenticate test e2e

---
+++ src/http/controllers/tests/authenticate.test.ts
@@ -0,0 +1,30 @@
+import request from "supertest";
+import { app } from "../../../app";
+import { afterAll, beforeAll, describe, expect, test } from "vitest";
+
+describe("Authenticate (e2e)", () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+  afterAll(async () => {
+    await app.close();
+  });
+
+  test("be able to authenticate", async () => {
+    await request(app.server).post("/users").send({
+      name: "John Doe",
+      email: "johndoe@example.com",
+      password: "123456",
+    });
+
+    const response = await request(app.server).post("/sessions").send({
+      email: "johndoe@example.com",
+      password: "123456",
+    });
+
+    expect(response.statusCode).toEqual(200);
+    expect(response.body).toEqual({
+      token: expect.any(String),
+    });
+  });
+});
