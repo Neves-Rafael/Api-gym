a3bcdff509e77815e11cec46c65af0af62e7d02f
Rafael Neves <nevesrafael.dev@gmail.com>
2024-05-22 21:23:11 -0300

    :sparkles: Feat user and gym create test

---
+++ src/http/controllers/gyms/tests/create.test.ts
@@ -0,0 +1,30 @@
+import request from "supertest";
+import { app } from "../../../../app";
+import { afterAll, beforeAll, describe, expect, test } from "vitest";
+import { createAndAuthenticateUser } from "../../../../utils/tests/create-and-authenticate-user";
+
+describe("Create Gym (e2e)", () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+  afterAll(async () => {
+    await app.close();
+  });
+
+  test("be able to create a gym", async () => {
+    const { token } = await createAndAuthenticateUser(app);
+
+    const response = await request(app.server)
+      .post("/gyms")
+      .set("Authorization", `Bearer ${token}`)
+      .send({
+        title: "JavaScript Gym",
+        description: "Some description",
+        phone: "61999999999",
+        latitude: -15.9393152,
+        longitude: -48.0307621,
+      });
+
+    expect(response.statusCode).toEqual(201);
+  });
+});
--- src/http/controllers/users/tests/profile.test.ts
+++ src/http/controllers/users/tests/profile.test.ts
@@ -1,6 +1,7 @@
 import request from "supertest";
 import { app } from "../../../../app";
 import { afterAll, beforeAll, describe, expect, test } from "vitest";
+import { createAndAuthenticateUser } from "../../../../utils/tests/create-and-authenticate-user";
 
 describe("Profile (e2e)", () => {
   beforeAll(async () => {
@@ -11,18 +12,7 @@ describe("Profile (e2e)", () => {
   });
 
   test("be able to get user profile", async () => {
-    await request(app.server).post("/users").send({
-      name: "John Doe",
-      email: "johndoe@example.com",
-      password: "123456",
-    });
-
-    const authResponse = await request(app.server).post("/sessions").send({
-      email: "johndoe@example.com",
-      password: "123456",
-    });
-
-    const { token } = authResponse.body;
+    const token = await createAndAuthenticateUser(app);
 
     const profileResponse = await request(app.server)
       .get("/me")
