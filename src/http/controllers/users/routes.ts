c4795285fda304fc3276de829fcea22dea163983
Rafael Neves <nevesrafael.dev@gmail.com>
2024-05-23 22:43:02 -0300

    :sparkles: Feat refresh controller

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -3,10 +3,13 @@ import { register } from "../../controllers/users/register";
 import { authenticate } from "../../controllers/users/authenticate";
 import { profile } from "../../controllers/users/profile";
 import { verifyJWT } from "../../middlewares/verify-jwt";
+import { refresh } from "./refresh";
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post("/users", register);
   app.post("/sessions", authenticate);
+
+  app.patch("/token/refresh", refresh);
 
   app.get("/me", { onRequest: [verifyJWT] }, profile);
 }
